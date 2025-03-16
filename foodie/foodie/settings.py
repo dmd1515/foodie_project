6d4f4bc1421a3681bb68964a1fbfd928f4043bda
Titas Buitkus <titas,buitkus@vilnius.eu>
2025-03-16 18:41:45 +0200

    Home screen UI design

--- foodie/foodie/settings.py
+++ foodie/foodie/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'foodie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'foodie_app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
