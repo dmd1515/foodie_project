7d91ec99ad9a473ed6907e5ff2c9cc579aa330a1
Deimante <deimburb@gmail.com>
2025-03-17 22:13:15 +0200

    Unit tests folder to create more unit tests

--- foodie/foodie_app/views.py
+++ foodie/foodie_app/views.py
@@ -7,12 +7,14 @@ from django.http import JsonResponse
 def home(request):
     return render(request, 'home.html')
 
+
 def upload_image(request):
-    if request.method == "POST" and request.FILES["image"]:
+    if request.method == "POST" and request.FILES.get("image"):
         image = request.FILES["image"]
         try:
             temp_image = TemporaryImage(image=image)
-            temp_image.save()
+            temp_image.full_clean()  # Užtikrina, kad bus atlikta validacija
+            temp_image.save()  # Įrašo į duomenų bazę
             return JsonResponse({"message": "Image uploaded successfully!"})
         except ValidationError as e:
             return JsonResponse({"error": str(e)}, status=400)
