f514dbde87d8c78000dd65a74a8cbf3e2d119745
Daniel Borkovski <113119069+litchi-main@users.noreply.github.com>
2025-04-13 20:43:14 +0300

    fixed unit test

--- foodie/foodie_app/recipe_generator_test.py
+++ foodie/foodie_app/recipe_generator_test.py
@@ -1,12 +1,12 @@
 import unittest
-from foodie_app.recipeGeneretro import sendPrompt
+from recipeGeneretro import sendPrompt
 
 class TestStringMethods(unittest.TestCase):
 
     def testRecipeGeneration(self):
-        testPrompt = "Reply by writing five /'a/' characters"
+        testPrompt = "Reply by writing only five /'a/' characters without an endline character"
         replyMessege = sendPrompt(testPrompt)
-        self.assertEqual(replyMessege, "aaaaa")
+        self.assertEqual(replyMessege, "aaaaa\n")
 
 if __name__ == '__main__':
     unittest.main()
