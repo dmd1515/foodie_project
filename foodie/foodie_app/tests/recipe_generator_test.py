c6c5be6e67d2eae93b5c104a0c9de5eb5e038d32
Daniel Borkovski <113119069+litchi-main@users.noreply.github.com>
2025-04-13 23:25:46 +0300

    testing pipeline #20 i think i got it added the milkshake test to diversify test case pool

--- foodie/foodie_app/tests/recipe_generator_test.py
+++ foodie/foodie_app/tests/recipe_generator_test.py
@@ -1,5 +1,5 @@
 import unittest
-from foodie_app.recipeGeneretro import sendPrompt
+from recipeGeneretro import sendPrompt
 
 class TestStringMethods(unittest.TestCase):
 
@@ -7,6 +7,11 @@ class TestStringMethods(unittest.TestCase):
         testPrompt = "Reply by writing only five /'a/' characters without an endline character"
         replyMessege = sendPrompt(testPrompt)
         self.assertEqual(replyMessege, "aaaaa\n")
+
+    def testMilkshake(self):
+        testPrompt = "Reply by writing only the first line of the song milkshake by kelis"
+        replyMessege = sendPrompt(testPrompt)
+        self.assertEqual(replyMessege.lower(), "my milkshake brings all the boys to the yard\n")
 
 if __name__ == '__main__':
     unittest.main()
