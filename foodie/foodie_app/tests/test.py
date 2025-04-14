c19b7660bebb874300377d1c713ec05e851b745f
Titas Buitkus <titas,buitkus@vilnius.eu>
2025-04-14 17:01:25 +0300

    Yml fix #4

--- foodie/foodie_app/tests/test.py
+++ foodie/foodie_app/tests/test.py
@@ -1,87 +1,164 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 import json
+from myapp.models import Zinute  # Tarkime, kad turite Zinute modelį
 
-class MessageTests(TestCase):
+class ZinuciuVienetiniaiTestai(TestCase):
+    """Vienetiniai testai žinučių validacijai ir apdorojimui"""
     def setUp(self):
         self.client = Client()
-        self.url = reverse('send_message')
-        self.valid_message = "This is a valid message"
-        self.long_message = "a" * 301  # 301 simboliai - viršija limitą
-        self.empty_message = ""
-        self.special_chars_message = "!@#$%^&*()_+{}|:\"<>?~`-=[]\\;',./"
-        self.max_length_message = "a" * 300  # Tiksliai 300 simbolių
+        self.nuoroda = reverse('siusti_zinute')
+        self.galima_zinute = "Tai yra tinkama žinutė"
+        self.per_ilga_zinute = "a" * 301
+        self.tuscia_zinute = ""
+        self.specialiu_simboliu_zinute = "!@#$%^&*()_+{}|:\"<>?~`-=[]\\;',./"
+        self.maksimalaus_ilgio_zinute = "a" * 300
 
-    def test_send_valid_message(self):
-        """Testuojama, ar galima siųsti tinkamo ilgio žinutę"""
-        response = self.client.post(
-            self.url,
-            {'message': self.valid_message},
+    def test_siusti_galima_zinute(self):
+        """Vienetinis testas: tinkamos žinutės siuntimas"""
+        atsakas = self.client.post(
+            self.nuoroda,
+            {'zinute': self.galima_zinute},
             HTTP_X_REQUESTED_WITH='XMLHttpRequest'
         )
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
-        self.assertEqual(data['success'], 'Message sent!')
+        self.assertEqual(atsakas.status_code, 200)
+        duomenys = json.loads(atsakas.content)
+        self.assertEqual(duomenys['pavyko'], 'Žinutė išsiųsta!')
 
-    def test_send_empty_message(self):
-        """Testuojama, ar negalima siųsti tuščios žinutės"""
-        response = self.client.post(
-            self.url,
-            {'message': self.empty_message},
+    def test_siusti_tuscia_zinute(self):
+        """Vienetinis testas: tuščios žinutės siuntimas"""
+        atsakas = self.client.post(
+            self.nuoroda,
+            {'zinute': self.tuscia_zinute},
             HTTP_X_REQUESTED_WITH='XMLHttpRequest'
         )
-        self.assertEqual(response.status_code, 400)
-        data = json.loads(response.content)
-        self.assertEqual(data['error'], 'Message cannot be empty.')
+        self.assertEqual(atsakas.status_code, 400)
+        duomenys = json.loads(atsakas.content)
+        self.assertEqual(duomenys['klaida'], 'Žinutė negali būti tuščia.')
 
-    def test_send_long_message(self):
-        """Testuojama, ar negalima siųsti per ilgos žinutės"""
-        response = self.client.post(
-            self.url,
-            {'message': self.long_message},
+
+class ZinuciuIntegraciniaiTestai(TestCase):
+    """Integraciniai testai tarp komponentų"""
+    def setUp(self):
+        self.client = Client()
+        self.siuntimo_nuoroda = reverse('siusti_zinute')
+        self.saraso_nuoroda = reverse('zinuciu_sarasas')
+        self.testine_zinute = "Integracinio testo žinutė"
+    
+    def test_zinutes_keliavimas_nuo_kliento_iki_duomenu_bazes(self):
+        """Integracinis testas: visas kelias nuo kliento iki duomenų bazės"""
+        # Siunčiame žinutę per API
+        atsakas = self.client.post(
+            self.siuntimo_nuoroda,
+            {'zinute': self.testine_zinute},
             HTTP_X_REQUESTED_WITH='XMLHttpRequest'
         )
-        self.assertEqual(response.status_code, 400, 
-                        "Turi grąžinti 400, kai žinutė viršija 300 simbolių")
-        data = json.loads(response.content)
-        self.assertEqual(data['error'], 'Message cannot exceed 300 characters.',
-                         "Turi grąžinti tinkamą klaidos pranešimą")
+        self.assertEqual(atsakas.status_code, 200)
+        
+        # Tikriname, ar žinutė išsaugota duomenų bazėje
+        zinute = Zinute.objects.first()
+        self.assertIsNotNone(zinute)
+        self.assertEqual(zinute.turinys, self.testine_zinute)
+        
+        # Tikriname, ar žinutė rodoma sąrašo API
+        saraso_atsakas = self.client.get(self.saraso_nuoroda)
+        self.assertEqual(saraso_atsakas.status_code, 200)
+        self.assertContains(saraso_atsakas, self.testine_zinute)
 
-    def test_send_max_length_message(self):
-        """Testuojama, ar galima siųsti maksimalaus ilgio žinutę (300 simbolių)"""
-        response = self.client.post(
-            self.url,
-            {'message': self.max_length_message},
+    def test_zinuciu_validavimas_tarp_lygiu(self):
+        """Integracinis testas: validacija tarp kliento ir serverio"""
+        # Testuojame per ilgą žinutę
+        per_ilga_zinute = "a" * 301
+        atsakas = self.client.post(
+            self.siuntimo_nuoroda,
+            {'zinute': per_ilga_zinute},
             HTTP_X_REQUESTED_WITH='XMLHttpRequest'
         )
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
-        self.assertEqual(data['success'], 'Message sent!')
-
-    def test_send_special_chars_message(self):
-        """Testuojama, ar galima siųsti žinutę su specialiais simboliais"""
-        response = self.client.post(
-            self.url,
-            {'message': self.special_chars_message},
+        self.assertEqual(atsakas.status_code, 400)
+        
+        # Tikriname, kad žinutė nebuvo išsaugota
+        self.assertEqual(Zinute.objects.count(), 0)
+        
+        # Testuojame tinkamą žinutę
+        galima_zinute = "Tinkama žinutė"
+        atsakas = self.client.post(
+            self.siuntimo_nuoroda,
+            {'zinute': galima_zinute},
             HTTP_X_REQUESTED_WITH='XMLHttpRequest'
         )
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.content)
-        self.assertEqual(data['success'], 'Message sent!')
+        self.assertEqual(atsakas.status_code, 200)
+        self.assertEqual(Zinute.objects.count(), 1)
 
-    def test_non_ajax_request(self):
-        """Testuojama, ar ne AJAX užklausos yra atmestos"""
-        response = self.client.post(
-            self.url,
-            {'message': self.valid_message}
+    def test_zinutes_apdorojimo_grandine(self):
+        """Integracinis testas: žinutės apdorojimas per kelis komponentus"""
+        # Siunčiame žinutę
+        testine_zinute = "Testinė žinutė"
+        atsakas = self.client.post(
+            self.siuntimo_nuoroda,
+            {'zinute': testine_zinute},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
         )
-        self.assertEqual(response.status_code, 400)
-        data = json.loads(response.content)
-        self.assertEqual(data['error'], 'Invalid request.')
+        self.assertEqual(atsakas.status_code, 200)
+        
+        # Tikriname duomenų bazę
+        zinute = Zinute.objects.first()
+        self.assertEqual(zinute.turinys, testine_zinute)
+        self.assertIsNotNone(zinute.laiko_zymė)
+        
+        # Tikriname, ar žinutė buvo apdorota (jei taip numatyta)
+        self.assertTrue(zinute.apdorota)
+        
+        # Tikriname, ar buvo sukurta pranešimą (jei taip numatyta)
+        # self.assertEqual(Pranesimas.objects.count(), 1)
 
-    def test_get_request(self):
-        """Testuojama, ar GET užklausos yra atmestos"""
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 400)
-        data = json.loads(response.content)
-        self.assertEqual(data['error'], 'Invalid request.')
+
+class ZinuciuAPIKomponenciuTestai(TestCase):
+    """Testai tarp skirtingų API komponentų"""
+    def setUp(self):
+        self.client = Client()
+        self.siuntimo_nuoroda = reverse('siusti_zinute')
+        self.statistika_nuoroda = reverse('zinuciu_statistika')
+    
+    def test_zinuciu_statistiku_integracija(self):
+        """Integracinis testas tarp žinučių ir statistikos komponentų"""
+        # Siunčiame kelias žinutes
+        zinutes = ["Testas 1", "Testas 2", "Testas 3"]
+        for zinute in zinutes:
+            atsakas = self.client.post(
+                self.siuntimo_nuoroda,
+                {'zinute': zinute},
+                HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+            )
+            self.assertEqual(atsakas.status_code, 200)
+        
+        # Tikriname statistiką
+        statistikos_atsakas = self.client.get(self.statistika_nuoroda)
+        self.assertEqual(statistikos_atsakas.status_code, 200)
+        statistikos_duomenys = json.loads(statistikos_atsakas.content)
+        
+        self.assertEqual(statistikos_duomenys['viso_zinuciu'], 3)
+        self.assertEqual(statistikos_duomenys['vidutinis_ilgis'], 6.0)
+        
+    def test_zinutes_ir_vartotojo_integracija(self):
+        """Integracinis testas tarp žinučių ir vartotojų komponentų"""
+        # Sukuriame testinį vartotoją
+        vartotojo_duomenys = {
+            'vartotojo_vardas': 'testuotojas',
+            'slaptazodis': 'testuotojoSlaptazodis123'
+        }
+        self.client.post(reverse('registruoti'), vartotojo_duomenys)
+        
+        # Prisijungiame
+        self.client.login(username='testuotojas', password='testuotojoSlaptazodis123')
+        
+        # Siunčiame žinutę kaip prisijungęs vartotojas
+        atsakas = self.client.post(
+            self.siuntimo_nuoroda,
+            {'zinute': "Vartotojo žinutė"},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+        )
+        self.assertEqual(atsakas.status_code, 200)
+        
+        # Tikriname vartotojo asociaciją
+        zinute = Zinute.objects.first()
+        self.assertEqual(zinute.vartotojas.vartotojo_vardas, 'testuotojas')
