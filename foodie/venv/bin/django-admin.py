6acdc8653edf2b9172c8206d6394b3d5579e6c2b
Deimante <deimburb@gmail.com>
2025-10-12 21:06:20 +0300

    Trying to sync for new code

--- foodie/venv/bin/django-admin.py
+++ foodie/venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/titasmac/Documents/foodie_project/foodie/venv/bin/python3
+#!/Users/deimanteburbaite/foodie_project/foodie/venv/bin/python3.13
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
