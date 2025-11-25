8623ff5e3003eb00fe341507b61a30beadb6b65e
Daniel Borkovski <113119069+litchi-main@users.noreply.github.com>
2025-11-25 20:00:51 +0200

    temp

--- foodie/foodie/urls.py
+++ foodie/foodie/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 from foodie_app import views
 from foodie_app.views import send_message  # Importuojame send_message view iš foodie_app aplikacijos
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/',  auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
     path('', views.home, name='home'),
     path('upload/', views.upload_image, name='upload_image'),
     path('send-message/', send_message, name='send_message'),
