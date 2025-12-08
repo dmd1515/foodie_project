6591f3a41a1e8898be4d92e7c3e744af9f155e60
Daniel Borkovski <113119069+litchi-main@users.noreply.github.com>
2025-12-08 22:03:27 +0200

    new prompt created

--- foodie/foodie_app/recipeGeneretro.py
+++ foodie/foodie_app/recipeGeneretro.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from google import genai
 import base64
+import json
 
 def generatePrompt(ingredients, mealTime, mealType, mealDiff, cookTime, useExtraIngredients):
     templatePrompt1 = "Generate 3 recipes using these ingredients: "
@@ -14,13 +15,18 @@ def generatePrompt(ingredients, mealTime, mealType, mealDiff, cookTime, useExtra
         templatePrompt2 += " meant to be cooked by " + mealDiff
     if(cookTime != "default"):
         templatePrompt2 += " that is made in " + cookTime
-    if(templatePrompt2 == ". These must be recipes "):
+    if(templatePrompt2 == ". These must be recipes"):
         templatePrompt2 = ""
     if(useExtraIngredients):
         templatePrompt2 += ". You can use other ingredients as well"
     else:
         templatePrompt2 += ". You must only use the mentioned ingredients and nothing more"
-    templatePrompt4 = ". Your recipes must include the name of the dish, cooking time, ingredients and instructions. Your reply text should only consist of the recipes and nothing more."
+    templatePrompt4 = ". Your recipes must include the name of the dish, cooking time, ingredients and instructions. " \
+    " Format the response in JSON with the keys: 'name', 'cookingTime', 'ingredients', and 'instructions'." \
+    " Values for 'ingredients' and 'instructions' should be lists." \
+    " Your response must be a valid JSON." \
+    " Here's a pattern of the expected format: " \
+    " { \"recipes\" : [ { \"name\": value, \"cookingTime\": value, \"ingredients\":[value1, value2, ...], \"instructions\": [value1, value2,...] }, ...] }"
     return templatePrompt1 + ingredients + templatePrompt2  + templatePrompt4
 
 def sendPrompt(data):
@@ -31,9 +37,9 @@ def sendPrompt(data):
     cookTime = data.get("cookTime", "default")
     useExtraIngredients = data.get("useExtraIngredients", False)
     prompt = generatePrompt(ingredients, mealTime, mealType, mealDiff, cookTime, useExtraIngredients)
-    #client = genai.Client(api_key=base64.b64decode("QUl6YVN5QWM3S2ZGYlF6RG5LajVwQ1FlcWpoWWFMLXd4WEliLXRR"))
+    client = genai.Client(api_key=base64.b64decode("QUl6YVN5QWM3S2ZGYlF6RG5LajVwQ1FlcWpoWWFMLXd4WEliLXRR"))
     #response = client.models.generate_content(
     #    model="gemini-2.5-flash", contents=prompt
     #)
-    prompt = {"response": "This is a mock response for the prompt: " + prompt}
+    #responseData = response.text
     return prompt
